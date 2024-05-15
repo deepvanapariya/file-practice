2dc75309c46aba73cd5e163fab3b46db29e3d036
Deep <deep.71.v@gmail.com>
2024-05-15 10:25:04 +0530

    for storage folder clear view

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const express = require("express");
 const ejs = require("ejs")
 const app = express();
 const path = require("path")
-const multer = require("multer")
+const multer = require("multer");
+const { error } = require("console");
 
 const PORT = 8080;
 
-const upload = multer({ dest: "uploads/" })
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null, "./uploads")
+    }, filename: function (req, file, cb) {
+        return cb(null, `${Date.now()}-${file.originalname}`)
+    }
+});
+
+const upload = multer({ storage })
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"))
-
 
 app.use(express.json());
 
@@ -21,7 +29,7 @@ app.get("/", (req, res) => {
 
 app.post("/upload", upload.single("profieImage"), (req, res) => {
     console.log(req.body)
-    console.log(req.file)
+    console.log(req.file.path)//can store in database and use it
     return res.redirect("/");
 
 })
